12e99060427e488a253eea8caebbf5a43d082554
arthuroi <parentoni.arthur@gmail.com>
2024-01-06 22:40:01 -0300

    Initial commit

---
+++ lua/parentoni/remap.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Allow visual selected to move
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Allow substitution
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Allow insertion of new line in normal mode
+vim.keymap.set("n", "<leader>o", "o<Esc>")
+
+-- Remap Esc 
+vim.keymap.set("i", "jk", "<Esc>")
+
+
+
+
---
+++ lua/parentoni/set.lua
@@ -0,0 +1,38 @@
+-- Line numbers configs
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Tab opt configs
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth= 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Line Configs
+vim.opt.wrap = false
+
+-- Backup and storage
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- Search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Colors
+
+-- Scroll and naviagtion
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.ve = "onemore"
+-- Screen
+vim.opt.termguicolors = true
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+
+-- Globals
+vim.g.mapleader = " "
