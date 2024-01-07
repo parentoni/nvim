a4e3c5f6d5c1035902e258aef42572880e0c1a3e
arthuroi <parentoni.arthur@gmail.com>
2024-01-06 22:43:45 -0300

    Added CSS support

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,7 +17,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer', "eslint", "tailwindcss", "lua_ls"},
+  ensure_installed = {'tsserver', 'rust_analyzer', "eslint", "tailwindcss", "lua_ls", "cssls"},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
